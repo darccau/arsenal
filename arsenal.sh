4d38bcf297b43317bcf5070e5e42bafd672db9ea
darccau <darccau@protonmail.com>
2022-06-07 23:54:20 -0300

    feat(arsenal): add initial tools

---
+++ arsenal.sh
@@ -0,0 +1,34 @@
+#!/usr/local/env bash
+
+go install -v github.com/projectdiscovery/cloudlist/cmd/cloudlist@latest
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+go install github.com/hahwul/dalfox/v2@latest
+go install -v github.com/tomnomnom/anew@latest
+go install -v github.com/projectdiscovery/interactsh/cmd/interactsh-client@latest
+go get https://github.com/tomnomnom/gf
+go get -u github.com/tomnomnom/gf
+go get -u github.com/tomnomnom/assetfinder
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+go install -v github.com/projectdiscovery/notify/cmd/notify@latest
+go install github.com/lc/gau/v2/cmd/gau@latest
+go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest
+
+
+pip3 install arjun
+
+git clone https://github.com/projectdiscovery/nuclei-templates
+
+git clone https://github.com/GerbenJavado/LinkFinder.git
+git https://github.com/003random/getJS
+
+git clone https://github.com/003random/getJS
+git clone github.com/sqlmapproject/sqlmap
+
+git clone https://github.com/techgaun/github-dorks
+
+
+
+
+
+
